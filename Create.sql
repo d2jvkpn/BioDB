f1781910f28d391343fe616932e1b39d58ceaa59
d2jvkpn <chenbin2018@protonmail.com>
2018-10-01 12:30:07 +0800

    Create Create.sql

---
+++ Create.sql
@@ -0,0 +1,46 @@
+-- author: d2jvkpn
+-- version: 0.1
+-- release: 2018-10-01
+-- project: https://github.com/d2jvkpn/BioDB
+-- license: GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
+
+
+create database BioDB;
+use BioDB;
+
+create table GO (
+    id           int              auto_increment,
+    GO_id        nchar(10)        not null,
+    prot_id      varchar(255),
+    class        nchar(1),
+    genes        varchar(255),
+    taxon_id     int              not null,
+    check        (taxon_id > 0),
+    primary key  (id)
+);
+
+
+create table TaxonID (
+    taxon_id         int           not null,
+    scientific_name  varchar(255)  not null,
+    common_name      varchar(255),
+    lineage          varchar(255),
+    primary key      (taxon_id)
+);
+
+
+create table Pathway (
+    code             varchar(63),
+    C_id             varchar(63)     not null,
+    C_name           varchar(255),       
+    A_id             varchar(63),
+    A_name           varchar(255),
+    B_id             varchar(63),
+    B_name           varchar(255),
+    gene_id          varchar(63)     not null,
+    gene_name        varchar(63),
+    gene_desciption  varchar(255),
+    KO_id             varchar(63),
+    KO_description   varchar(255),
+    EC_id            varchar(63)
+);
