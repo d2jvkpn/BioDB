fa011f3035361be20b8d8b8f10958e8c95ac7be3
d2jvkpn <chenbin2018@protonmail.com>
2018-10-03 09:16:50 +0800

    Create Table_Taxon_v0.1.go

---
+++ Table_Taxon_v0.1.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+	"io"
+	"log"
+	"encoding/csv"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+
+func main () {
+	db, err := sql.Open("mysql", "hello:world@/BioDB")
+	if err != nil { log.Fatal (err) }
+	defer db.Close()
+
+	stmt, err := db.Prepare (`insert into Taxon 
+	(taxon_id, scientific_name) values (?, ?)`)
+
+	if err != nil { log.Fatal(err) }
+
+	rd := csv.NewReader (os.Stdin)
+    rd.Comma, rd.Comment, rd.FieldsPerRecord  = '\t', '!', -1
+
+	for {
+		record, err := rd.Read ()
+		if err == io.EOF { break }
+		if err != nil { log.Println (err); continue }
+
+		
+		_, err = stmt.Exec (record[0], record[1])
+		if err != nil {log.Println (err)}
+	}
+}
