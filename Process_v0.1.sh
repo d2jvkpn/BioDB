8e13120e55e70f7f5c4ba6fa6b212f0e594599a0
d2jvkpn <chenbin2018@protonmail.com>
2018-10-09 23:53:10 +0800

    Update Process_v0.1.sh

--- Process_v0.1.sh
+++ Process_v0.1.sh
@@ -4,13 +4,14 @@
 
 # wget ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz
 # mkdir taxdump
-# tar -xf taxdump.tar.gz -C taxdump
+# tar -xf taxdump.tar.gz names.dmp
 
-# 130542438 records
-pigz -dc goa_uniprot_all.gaf.gz | go run Table_GO_v0.4.go
+# 130,542,438 records
+# 513,365,944
+pigz -dc goa_uniprot_all.gaf.gz | go run BuildGO_v0.5.go
 
 
-# 1858425 records
-sed 's/\t|\t/\t/g; s/|$//' taxdump/names.dmp |
+# 1,858,425 records
+sed 's/\t|\t/\t/g; s/|$//' names.dmp |
 awk 'BEGIN{FS=OFS="\t"} $4=="scientific name"{print $1, $2}' |
-go run Table_Taxon_v0.2.go
+go run BuildTaxon_v0.2.go
