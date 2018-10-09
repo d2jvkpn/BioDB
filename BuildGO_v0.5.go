f71995f66f457ce7a02203b166eccd3ef785ebef
d2jvkpn <chenbin2018@protonmail.com>
2018-10-09 23:46:24 +0800

    Update and rename BuildGO_v0.4.go to BuildGO_v0.5.go

--- BuildGO_v0.5.go
+++ BuildGO_v0.5.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"io"
 	"log"
+	"fmt"
 	"sync"
 	"strings"
 	"encoding/csv"
@@ -13,7 +14,7 @@ import (
 
 
 func main () {
-	db, err := sql.Open("mysql", "hello:world@/BioDB")
+	db, err := sql.Open("mysql", "hello:@/BioDB")
 	if err != nil { log.Fatal (err) }
 	defer db.Close()
 
@@ -26,14 +27,21 @@ func main () {
 	rd.Comma, rd.Comment, rd.FieldsPerRecord  = '\t', '!', -1
 	var wg sync.WaitGroup
 	ch := make (chan struct{}, 50)
+	tcount := make (map[string] bool)
+	taxon_id, rcount := "", 0
 
 	for {
 		record, err := rd.Read ()
 		if err == io.EOF { break }
 		if err != nil { log.Println (err); continue }
 
-		rc := []string {record[4], record[7], record[8],
-		record[10], strings.TrimLeft (record[12], "taxon:")}
+		if record[10] == "" { continue } // skip record when genes is empty string
+
+		taxon_id = strings.TrimLeft (record[12], "taxon:")
+		tcount[taxon_id] = true
+		rcount ++
+		if rcount > 1000000 { break } // import partly data for testing
+		rc := []string {record[4], record[7], record[8], record[10], taxon_id}
 
 		ch <- struct {}{}
 		wg.Add (1)
@@ -41,12 +49,15 @@ func main () {
 	}
 
 	wg.Wait()
+
+	fmt.Printf("%d GO records of %d taxonomies imported\n", rcount-1, len(tcount))
 }
 
 func Insert (rc []string, stmt *sql.Stmt, 
 	wg *sync.WaitGroup, ch <- chan struct{}) {
 
 	defer func () { <- ch; wg.Done() }()
+
 	_, err := stmt.Exec (rc[0], rc[1], rc[2], rc[3], rc[4])
 	if err != nil {log.Println (err)}
 }
