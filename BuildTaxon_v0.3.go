8e8de6f0261f214dabfa648e677c3b74da5e0a2c
d2jvkpn <chenbin2018@protonmail.com>
2018-11-06 22:52:40 +0800

    Update and rename BuildTaxon_v0.2.go to BuildTaxon_v0.3.go

--- BuildTaxon_v0.3.go
+++ BuildTaxon_v0.3.go
@@ -4,11 +4,13 @@ import (
 	"os"
 	"io"
 	"log"
+	"fmt"
 	"strings"
 	"encoding/csv"
 	"net/url"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"time"
 )
 
 
@@ -26,13 +28,20 @@ func main () {
 	rd.Comma, rd.Comment, rd.FieldsPerRecord  = '\t', '!', -1
 	rd.LazyQuotes = true
 
+	var i int
+	StartAt := time.Now()
+
 	for {
 		record, err := rd.Read ()
 		if err == io.EOF { break }
 		if err != nil { log.Println (err); continue }
+		
+		i++
 
 		record[1] = url.QueryEscape (strings.ToLower (record[1]))
 		_, err = stmt.Exec (record[0], record[1])
 		if err != nil {log.Println (err)}
 	}
+
+	fmt.Println("%d records have be imported in %s", i, time.Now().Sub(StartAt))
 }
