b035bc92b5af0e9af21916471a0462cd424322e8
d2jvkpn <chenbin2018@protonmail.com>
2018-11-10 23:49:32 +0800

    Update LoadGOSeperatly.sh

--- LoadGOSeperatly.sh
+++ LoadGOSeperatly.sh
@@ -2,9 +2,11 @@
 
 set -eu -o pipefail
 
+date
 pigz -dc GO.tsv.gz | sed '1d' | split -l 50000000 --additional-suffix=.GO.tsv
 # xaa.GO.tsv ... xak.GO.tsv
 
+date
 {
   for i in x*.GO.tsv; do
     echo "load data local infile '$i' into table BioDB.GO (taxon_id, genes, GO_id);"
@@ -13,3 +15,11 @@ pigz -dc GO.tsv.gz | sed '1d' | split -l 50000000 --additional-suffix=.GO.tsv
 } > LoadGO.sql
 
 mysql -u hello < LoadGO.sql
+date
+
+exit
+
+-- Query OK, 49999635 rows affected, 365 warnings (5 min 42.88 sec)
+-- Records: 50000000  Deleted: 0  Skipped: 365  Warnings: 365
+
+show warnings;
