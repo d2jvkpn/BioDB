4d7911379d787530a2beed9218b16a0a21876e19
d2jvkpn <chenbin2018@protonmail.com>
2018-11-12 23:22:23 +0800

    Update and rename scientific_name_url_quote.py to scientific_name_QueryEscape.go

---
+++ scientific_name_QueryEscape.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/url"
+	"os"
+	"log"
+	"bufio"
+	"strings"
+)
+
+func main () {
+	frd, err := os.Open("Taxonomy0.tsv")
+	if err != nil {log.Fatal(err)}
+	defer frd.Close()
+	r := bufio.NewScanner(frd)
+
+	if _, err := os.Stat("Taxonomy.tsv"); !os.IsNotExist(err) {
+		err := os.Remove("Taxonomy.tsv")
+		if err != nil {log.Fatal(err)}
+	}
+
+	fwt, err := os.Create("Taxonomy.tsv")
+	if err != nil {log.Fatal(err)}
+	defer fwt.Close()
+
+	var l, nc string
+	r.Scan()
+	l = r.Text()
+	fwt.Write([]byte(l + "\tescape_name\n"))
+
+	for r.Scan() {
+		l = r.Text()
+		nc = strings.ToLower(strings.Split(l, "\t")[1])
+		fwt.Write([]byte(l + "\t" + url.QueryEscape(nc) + "\n"))
+	}
+}
--- scientific_name_url_quote.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from urllib.parse import quote
-
-td = pd.read_csv("Taxonomy0.tsv", sep="\t", header=0, index_col=0)
-
-td["escape_name"] = [quote(i.lower()) for i in td["scientific_name"]]
-
-td.to_csv("Taxonomy.tsv", sep="\t")
