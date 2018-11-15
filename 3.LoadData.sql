08192001c9276001536393542d64588b22bb7fa0
d2jvkpn <chenbin2018@protonmail.com>
2018-11-15 22:06:53 +0800

    Update 3.LoadData.sql

--- 3.LoadData.sql
+++ 3.LoadData.sql
@@ -4,10 +4,39 @@ load data local infile 'Taxonomy.tsv' into table BioDB.Taxonomy ignore 1 lines
 -- Records: 2003451  Deleted: 0  Skipped: 0  Warnings: 0
 
 alter table BioDB.Taxonomy add index escape_name (escape_name);
--- Query OK, 0 rows affected, 1 warning (8.97 sec)     
--- Records: 0  Duplicates: 0  Warnings: 1
 
+load data local infile 'Homotypic_synonym.tsv' into table BioDB.homotypic 
+ignore 1 lines (taxon_id, name);
+
+alter table BioDB.homotypic add index taxon_id (taxon_id);
+alter table BioDB.homotypic add index name (name);
 
 
 load data local infile 'GO.tsv' into table BioDB.GO ignore 1 lines
 (taxon_id, genes, GO_id);
+-- 349,174,064 records
+
+alter table BioDB.GO add index taxon_id (taxon_id);
+
+
+load data local infile 'Pathway.tsv' into table BioDB.Pathway ignore 1 lines;
+-- Query OK, 0 rows affected, 65535 warnings (7 min 38.73 sec)
+-- Records: 10815282  Deleted: 0  Skipped: 10815282  Warnings: 28708929
+
+alter table BioDB.Pathway add index taxon_id (taxon_id);
+alter table BioDB.Pathway add index pathway_id (pathway_id);
+
+
+load data local infile 'Pathway_Definition.tsv' into table 
+BioDB.Pathway_definition ignore 1 lines;
+
+alter table BioDB.pathway_definition add index id (id);
+
+exit
+
+select * from Pathway order by taxon_id limit 100;
+
+create table P2 like Pathway;
+insert into P2 select * from Pathway order by taxon_id;
+drop table Pathway;
+rename table P2 to Pathway;
