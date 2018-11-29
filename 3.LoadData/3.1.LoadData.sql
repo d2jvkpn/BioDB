85b74e150f89c97071ca99c60173df4b025c39f8
d2jvkpn <chenbin2018@protonmail.com>
2018-11-29 23:54:14 +0800

    Update 3.1.LoadData.sql

--- 3.LoadData/3.1.LoadData.sql
+++ 3.LoadData/3.1.LoadData.sql
@@ -4,11 +4,10 @@ ignore 1 lines (taxon_id, scientific_name, taxon_rank, parent_id, escape_name);
 -- Query OK, 2003451 rows affected (15.47 sec)          
 -- Records: 2003451  Deleted: 0  Skipped: 0  Warnings: 0
 
-alter table BioDB.Taxonomy add index escape_name (escape_name);
-
 load data local infile 'data_Taxonomy/Homotypic_synonym.tsv' into 
 table BioDB.Taxonomy_homotypic ignore 1 lines (taxon_id, name);
 
+alter table BioDB.Taxonomy add index escape_name (escape_name);
 alter table BioDB.Taxonomy_homotypic add index taxon_id (taxon_id);
 alter table BioDB.Taxonomy_homotypic add index name (name);
 
