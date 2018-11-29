242efac092c4ec9d2efeb66311d6dfbe92c0c3d4
d2jvkpn <chenbin2018@protonmail.com>
2018-11-29 22:56:15 +0800

    Delete scientific_name_url_quote.py

--- scientific_name_url_quote.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from urllib.parse import quote_plus
-import csv
-
-td = pd.read_csv("Taxonomy.0.tsv", sep="\t", header=0, index_col=0, dtype=str)
-td["escape_name"] = [quote_plus(i.lower()) for i in td["scientific_name"]]
-td.to_csv("Taxonomy.tsv", sep="\t",  quoting = csv.QUOTE_NONE)
